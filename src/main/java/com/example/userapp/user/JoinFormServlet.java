3601d2a6c7cad2d481ef3dd029ff108841df622c
LifeIsOne <didtmdgh258@gmail.com>
2024-01-25 09:16:19 +0900

    환경설정완료

---
+++ src/main/java/com/example/userapp/user/JoinFormServlet.java
@@ -0,0 +1,9 @@
+package com.example.userapp.user;
+
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+
+@WebServlet("/join-form")
+public class JoinFormServlet extends HttpServlet {
+
+}
